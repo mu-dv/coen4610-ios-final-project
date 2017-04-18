d802f7713f5a0a4b52fadf52ccdfb1427338ce07
Austin Anderson <austinanderson08@gmail.com>
2017-04-18 15:38:28 -0500

    Initial commit

---
+++ AstroidsMarquette/LaunchScreenViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AstroidsMarquette
+//
+//  Created by Austin Anderson on 4/4/17.
+//  Copyright © 2017 austinandersonphoto. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
+    }
+}
+
