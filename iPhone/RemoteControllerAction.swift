87903a32151c7de605781a12be3ce6f6bf48b10a
Vanderwiel <2855vanderd@COE-MAC-2101.local>
2017-04-29 15:50:08 -0500

    Cleaned the controller up

---
+++ iPhone/RemoteControllerAction.swift
@@ -0,0 +1,19 @@
+//
+//  RemoteControllerAction.swift
+//  InvadersController
+//
+//  Created by Vanderwiel, Drew on 4/29/17.
+//  Copyright © 2017 Connery, Courtney. All rights reserved.
+//
+
+import Foundation
+
+enum RemoteControllerAction
+{
+    case MoveLeftDown
+    case MoveLeftUp
+    case MoveRightDown
+    case MoveRightUp
+    case FireDown
+    case FireUp
+}
